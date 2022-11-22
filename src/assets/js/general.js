e7733a40714c3b256ee1c152d54c32f95baec9cb
Dirk <99747924+Kerraan@users.noreply.github.com>
2022-11-22 22:28:57 +0100

    updated nav menu

--- src/assets/js/general.js
+++ src/assets/js/general.js
@@ -1,4 +1,18 @@
 "use strict";
 
-console.log("hello world");
-console.log("hellow wordl");
+const primaryNav = document.querySelector(".mainnav");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+    navToggle.classList.add("open");
+  } else if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+    navToggle.classList.remove("open");
+  }
+});
